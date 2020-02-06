034c19d563985a3cb40957dfbe09a6c8d5f3de8a
Joey Chen <jay.hatelove@gmail.com>
2020-02-06 18:59:33 +0800

    add specflow tests

---
+++ AccountingWebTests/CheckEnvironment/CheckSpecflowSteps.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using ExpectedObjects;
+using TechTalk.SpecFlow;
+using static TechTalk.SpecFlow.ScenarioContext;
+
+#pragma warning disable 618
+
+namespace AccountingWebTests.CheckEnvironment
+{
+    [Binding]
+    public class CheckSpecflowSteps:Steps
+    {
+        private Calculator _calculator;
+
+        [BeforeScenario()]
+        public void BeforeScenario()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            _calculator.Add(number);
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int expected)
+        {
+            var actual = ScenarioContext.Get<int>("result");
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            var result = _calculator.Result();
+            ScenarioContext.Set(result, "result");
+        }
+    }
+
+    public class Calculator
+    {
+        private int _sum;
+
+        public void Add(int number)
+        {
+            _sum += number;
+        }
+
+        public int Result()
+        {
+            return _sum;
+        }
+    }
+}
