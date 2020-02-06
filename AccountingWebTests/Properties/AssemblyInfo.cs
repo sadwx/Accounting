e8fd89352f968fbfa14703f98276b26b5c5c7519
Joey Chen <jay.hatelove@gmail.com>
2020-02-06 17:19:07 +0800

    add web testing project by Atata project template, and add nuget package reference

--- AccountingWebTests/Properties/AssemblyInfo.cs
+++ AccountingWebTests/Properties/AssemblyInfo.cs
@@ -1,8 +1,7 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AccountingWebTests")]
@@ -14,22 +13,23 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2bd4b0d5-bad0-4e64-ad4a-6d8993f39201")]
+[assembly: Guid("428fcad3-c5df-4b87-9041-074cab2c1d2c")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AccountingWebTests/UITestFixture.cs
@@ -0,0 +1,29 @@
+﻿using Atata;
+using NUnit.Framework;
+
+namespace AccountingWebTests
+{
+    [TestFixture]
+    public class UITestFixture
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            // Find information about AtataContext set-up on https://atata.io/getting-started/#set-up
+            AtataContext.Configure()
+                        .UseChrome()
+                        //    WithArguments("start-maximized").
+                        //UseBaseUrl("http://automationpractice.com/index.php")
+                        .UseCulture("en-us").UseNUnitTestName()
+                        .AddNUnitTestContextLogging().LogNUnitError()
+                        .UseAssertionExceptionType<NUnit.Framework.AssertionException>()
+                        .UseNUnitAggregateAssertionStrategy().Build();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            AtataContext.Current?.CleanUp();
+        }
+    }
+}
